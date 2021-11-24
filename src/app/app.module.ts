430e0dfd68f8c9c3c7e91e4eaeb0d6b99b4cca1e
Thiago-M-Silva <thiagobossun06@gmail.com>
2021-11-24 17:25:21 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { VeiculoComponent } from './veiculo/veiculo.component';
+import { VeiculoDetalhesComponent } from './veiculo-detalhes/veiculo-detalhes.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, VeiculoComponent],
+  declarations: [AppComponent, VeiculoComponent, VeiculoDetalhesComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
